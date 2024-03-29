47e42c2c33b2884413e9a8ceae117c72319254ab
ValeriyCh04 <164645172+ValeriyCh04@users.noreply.github.com>
2024-03-29 07:23:38 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,11 +1,9 @@
 name = 'Valeriy'
-print('Name: ' + name)
+print('Name: ', name)
 age = 32
-print('Age:', + age)
+print('Age:', age)
 age = age+1
-print('New age:', + age)
-is_student = 'True'
-print('Is student: ' + is_student)
-
-
-
+print('New age:', age)
+is_student = True
+print(type(is_student))
+print('Is student:', is_student)
